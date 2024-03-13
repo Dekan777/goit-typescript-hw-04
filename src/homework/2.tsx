97e6e53afcae524c730501df03e95365cdd7428e
Denis Koshevyi <denis.koshevoy1987@gmail.com>
2024-03-13 23:38:11 +0100

    Action

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -5,6 +5,12 @@ type State = {
   requestStep: "start" | "pending" | "finished" | "idle";
 };
 
+type Action =
+  | { type: "START_REQUEST" }
+  | { type: "PENDING_REQUEST" }
+  | { type: "FINISH_REQUEST" }
+  | { type: "RESET_REQUEST" };
+
 const initialState: State = {
   isRequestInProgress: false,
   requestStep: "idle",
